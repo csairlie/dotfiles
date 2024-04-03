4256ee0acc2359fc338c9ab0b67fff5c1cccefd8
Callum Airlie <143629409+csairlie@users.noreply.github.com>
2024-04-03 11:59:00 -0400

    Updated lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,15 @@ return {
     require("lualine").setup({
       options = {
         theme = 'horizon',
-      },
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch'},
+            lualine_c = {'filename'},
+            lualine_x = {'filetype'},
+            lualine_y = {},
+            lualine_z = {"os.date('%I:%M%p')"}
+        },
     })
   end,
 }
