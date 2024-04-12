975a7b6665e2b74df4885bf0bfee6008db3a9902
Callum Airlie <143629409+csairlie@users.noreply.github.com>
2024-04-12 00:32:31 -0400

    Updated neovim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,16 +1,16 @@
--- function for quicker keymapping --
-local function map(mode, lhs, rhs)
-  local options = { noremap=true, silent=true }
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
--- Remap space as leader key
-map("", "<Space>", "<nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Disable arrow keys
-map('', '<up>', '<nop>', opts)
-map('', '<down>', '<nop>', opts)
-map('', '<left>', '<nop>', opts)
-map('', '<right>', '<nop>', opts)
+local keymap = vim.keymap
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
